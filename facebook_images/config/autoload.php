1da6fc3f970aeb2bb4c15677831fd3925828a2ee
Bandyt <Bandyt@gmx.net>
2013-01-09 15:42:47 +0100

    ADDED: Contao 3.x support

---
+++ facebook_images/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package Facebook_images
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'facebook_images' => 'system/modules/facebook_images/facebook_images.php',
+));
